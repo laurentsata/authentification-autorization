d2f30827c5af87f27986e42bfa4ee6ed8a2ea5d3
laurentsata <sataward666@gmail.com>
2022-10-14 16:27:07 +0200

    ap port => port

--- frontend/src/pages/AdminProductPage.jsx
+++ frontend/src/pages/AdminProductPage.jsx
@@ -13,7 +13,7 @@ export default function AdminProductPage() {
     name: "",
     price: "",
     image: "",
-    describe: "",
+    description: "",
   });
 
   const [selectedCategories, setSelectedCategories] = useState("");
